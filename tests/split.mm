07edcb28a9b6392658b0bb3375d7d5771feab707
mizt <info@mizt.org>
2023-03-12 07:25:11 +0900

    Update

--- tests/split.mm
+++ tests/split.mm
@@ -1,41 +1,45 @@
 #import <Foundation/Foundation.h>
+#import "../MultiTrackQTMovie.h"
 #import "../MultiTrackQTMovieParser.h"
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
         
-        if(argc==2) {
-            NSString *SRC = [NSString stringWithFormat:@"%s",argv[1]];
-            NSLog(@"%@",SRC);
-            MultiTrackQTMovie::Parser *parser = new MultiTrackQTMovie::Parser(SRC);
-            
-            for(int n=0; n<parser->tracks(); n++) {
-                
-                NSString *fn = [NSString stringWithFormat:@"%@%@.mov",[SRC stringByDeletingPathExtension],(parser->type(n)=="png ")?@"-pointcloud":@"-texture"];
-                
-                unsigned short w = parser->width(n);
-                unsigned short h = parser->height(n);
-                unsigned short depth = parser->depth(n);
-                double FPS = parser->FPS(n);
-                
-                std::vector<MultiTrackQTMovie::TrackInfo> info;
-                info.push_back({.width=w,.height=h,.depth=depth,.fps=FPS,.type=parser->type(n)});
-                
-                MultiTrackQTMovie::Recorder *recorder = new MultiTrackQTMovie::Recorder(fn,&info);
-                
-                int len = parser->length(n);
-                for(int k=0; k<len; k++) {
-                    NSData *image = parser->get(k,n);
-                    recorder->add((unsigned char *)[image bytes],(unsigned int)[image length],0);
-                }
-                recorder->save();
-                
-            }
-
-        }
-        else {
-            NSLog(@"Err: Require a path as argument");
-        }
+        MultiTrackQTMovie::Parser *parser = new MultiTrackQTMovie::Parser(@"./test.mov");
         
+        for(int n=0; n<parser->tracks(); n++) {
+            
+            __block bool wait = true;
+            
+            NSString *fn = [NSString stringWithFormat:@"%02d.mov",n];
+            
+            unsigned short w = parser->width(n);
+            unsigned short h = parser->height(n);
+            unsigned short depth = parser->depth(n);
+            double FPS = parser->FPS(n);
+            
+            std::vector<MultiTrackQTMovie::TrackInfo> info;
+            info.push_back({.width=w,.height=h,.depth=depth,.fps=FPS,.type=parser->type(n)});
+            
+            MultiTrackQTMovie::Recorder *recorder = new MultiTrackQTMovie::Recorder(fn,&info);
+            
+            EventEmitter::on(MultiTrackQTMovie::Event::SAVE_COMPLETE,^(NSNotification *notification) {
+                EventEmitter::off(MultiTrackQTMovie::Event::SAVE_COMPLETE);
+                delete recorder;
+                wait = false;
+            });
+            
+            int len = parser->length(n);
+            for(int k=0; k<len; k++) {
+                NSData *image = parser->get(k,n);
+                recorder->add((unsigned char *)[image bytes],(unsigned int)[image length],0);
+            }
+            
+            recorder->save();
+            
+            while(wait) {
+                [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0f]];
+            }
+        }
     }
 }
